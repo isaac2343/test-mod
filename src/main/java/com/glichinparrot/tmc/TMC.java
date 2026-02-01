e95475c40378609c907f8fcc4093162b12ed586e
Isaac Norton <isaacnorton1911@gmail.com>
2026-02-01 10:05:12 -0500

    item

--- src/main/java/com/glichinparrot/tmc/TMC.java
+++ src/main/java/com/glichinparrot/tmc/TMC.java
@@ -1,14 +1,16 @@
 package com.glichinparrot.tmc;
 
+import com.glichinparrot.tmc.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TMC implements ModInitializer {
- public static final String Mod_ID = "TMC";
+ public static final String Mod_ID = "tmc";
  public static final  Logger LOGGER = LoggerFactory.getLogger(Mod_ID);
 	@Override
 	public void onInitialize() {
+        ModItems.registerModItems();
 	}
 }
